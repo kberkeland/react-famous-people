65ba5d5c7577ddffe71ce70696686039a5933bbb
Kye Berkeland <kyeberkeland@yahoo.com>
2019-01-15 15:44:31 -0600

    Add famous person and input fields

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = { person: 'Franklin Roosevelt',
+                   role: 'President'
+                 }
+  }
+
+  handlePersonInput = (event) => {
+
+    this.setState({person: event.target.value});
+
+  }
+
+  handleRoleInput = (event) => {
+
+    this.setState({role: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>React Famous People</h1>
         </header>
+        <p>Person = {this.state.person}</p>
+        <input onChange={this.handlePersonInput}></input>
+        <p>Role = {this.state.role}</p>
+        <input onChange={this.handleRoleInput}></input>
       </div>
     );
   }
